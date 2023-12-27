d8987f977a9aabaf0bd5caac6ef3a56bbbeef9d2
0xmrwn <niches-anode0k@icloud.com>
2023-12-27 18:33:59 +0100

    added script to parse markdown into yaml for course structure

---
+++ courses/seconde/maths/structure/raw_structure.md
@@ -0,0 +1,224 @@
+# Maths Seconde 2019
+
+## C1 : Nombres entiers, nombres réels
+
+### C1S1: Nombres entiers
+
+1. Nombres entiers naturels et nombres entiers relatifs
+2. Multiples et diviseurs
+
+### C1S2: Nombres réels
+
+1. Nombres décimaux et nombres rationnels
+2. L’ensemble des nombres réels
+
+### C1S3: Intervalles - Valeur absolue d’un nombre réel
+
+1. Les intervalles de R
+2. Valeur absolue d’un nombre réel
+
+### C1S4: Puissances
+
+1. Définition d’une puissance
+2. Calculs avec des puissances
+3. Écriture scientifique d’un nombre
+
+## C2: Équations et inéquations
+
+### C2S1: Égalités et équations
+
+1. Propriétés des égalités
+2. Équations
+3. Identités remarquables
+
+### C2S2: Inégalités et inéquations
+
+1. Propriété des inégalités
+2. Encadrement d’un nombre réel et arrondis
+3. Inéquations
+
+## C3: Fonctions affines
+
+### C3S1: Fonctions affines
+
+1. Définition
+2. Représentation graphique
+
+### C3S2: Variations et signe d’une fonction affine
+
+1. Variations d’une fonction affine
+2. Signe d’une fonction affine
+
+## C4: Fonctions carré et cube
+
+### C4S1: Fonction carré
+
+1. Définition
+2. Représentation graphique
+3. Variations de la fonction carré
+
+### C4S2: Fonction cube
+
+1. Définition
+2. Représentation graphique
+3. Variations de la fonction cube
+
+### C4S3: Équations et inéquations
+
+1. Résolution des équations x^2 = k
+2. Réunion d’intervalles
+3. Résolution des inéquations x^2 ≤ k et x^2 ≥ k
+
+### C4S4: Inéquations produit
+
+1. Résolution des inéquations produit
+2. Position relative des courbes d’équation y = x , y = x^2 et y = x^3
+
+## C5: Fonctions racine carrée et inverse
+
+### C5S1: Fonction racine carrée
+
+1. Définition
+2. Représentation graphique
+3. Variation de la fonction racine carrée
+4. Propriété algébriques de la racine carrée
+
+### C5S2: Fonction inverse
+
+1. Définition et propriété
+2. Variation de la fonction inverse
+
+### C5S3: Équations et inéquations avec la fonction racine carrée
+
+1. Résolution de l’équation √x = k
+2. Résolution des inéquations √x <= k et √x >= k
+
+### C5S4: Équations et inéquations avec la fonction inverse
+
+1. Résolution de l’équation 1/x = a
+2. Résolution de l’inéquation 1/x ≤ a
+3. Résolution des inéquations du type A(x) / B(x) ≤ 0 (ou ≥ 0)
+
+## C6: Variations et extremums des fonctions
+
+### C6S1: Parité d’une fonction
+
+1. Fonction paire, fonction impaire
+2. Représentation graphique des fonctions paires et impaires
+
+### C6S2: Variation d’une fonction et extremums
+
+1. Fonction croissante, décroissante
+2. Étude des variations
+3. Maximum, minimum (extremum) d’une fonction
+
+## C7: Vecteurs
+
+### C7S1: Notion de vecteur
+
+1. Vecteur et translation
+2. Vecteurs égaux
+3. Opposé d’un vecteur
+
+### C7S2: Opérations sur les vecteurs
+
+1. Somme de deux vecteurs
+2. Produit d’un vecteur par un réel
+
+### C7S3: Coordonnées d’un vecteur
+
+1. Base orthonormée
+2. Coordonnées d’un vecteur AB
+
+### C7S4: Coordonnées et opérations
+
+1. Norme d’un vecteur
+2. Somme de deux vecteurs
+3. Produit d’un vecteur par un réel
+
+## C8: Problèmes de géométrie
+
+### C8S1: Colinéarité
+
+1. Vecteurs colinéaires
+2. Déterminant de deux vecteurs
+
+### C8S2: Parallélisme et alignement
+
+1. Droites parallèles
+2. Points alignés
+3. Milieu d’un segment
+
+### C8S3: Orthogonalité
+
+1. Relations trigonométriques dans un triangle rectangle
+2. Projeté orthogonal d’un point sur une droite
+
+## C9: Droites et systèmes
+
+### C9S1: Équation cartésienne de droite
+
+1. Vecteur directeur d’une droite
+2. Équation cartésienne de droite
+
+### C9S2: Équation réduite de droite
+
+1. Droite parallèle à l’axe des ordonnées
+2. Droite non parallèle à l’axe des ordonnées
+
+### C9S3: Systèmes d’équations
+
+1. Système linéaire de deux équations à 2 inconnues
+2. Lien entre les droites et les systèmes
+
+## C10: Information chiffrée et statistiques descriptives
+
+### C10S1: Proportion et pourcentage
+
+1. Population et sous-population
+2. Proportion d’une sous-population
+3. Pourcentage de pourcentage
+
+### C10S2: Variations d’une quantité
+
+1. Variation absolue
+2. Variation relative
+
+### C10S3: Évolution d’une quantité
+
+1. Évolution successives
+2. Évolution réciproque
+
+### C10S4: Indicateurs de séries statistiques
+
+1. Indicateurs de tendance centrale
+2. Indicateurs de dispersion
+
+## C11: Probabilités
+
+### C11S1: Expérience aléatoire
+
+1. Expérience aléatoire et vocabulaire
+2. Modélisation d’une expérience aléatoire
+
+### C11S2: Calcules et probabilités
+
+1. Probabilité d’un évènement
+2. Réunion et intersection d’évènements
+
+### C11S3: Dénombrement
+
+1. Tableau à double entrée
+2. Arbre
+
+## C12: Échantillonnage
+
+### C12S1: Échantillon
+
+1. Notion d’échantillon
+2. Influence de la taille de l’échantillon
+
+### C12S2: Principe d’estimation
+
+1. Fluctuation d’échantillonnage
+2. Estimation d’une proportion
--- source/utils.py
+++ source/utils.py
@@ -1,79 +1,115 @@
+import os
 import re
+import sys
 from collections import OrderedDict
 
 import yaml
+from loguru import logger
+
+logger.add(
+    sys.stdout,
+    colorize=True,
+    format="<green>{time}</green> <level>{message}</level>",
+    filter="my_module",
+)
 
 
-def parse_markdown(file_path):
-    with open(file_path, "r") as file:
-        content = file.readlines()
-
-    main_topic_pattern = re.compile(r"^##\s+(.+)")  # Pattern for main topics
-    subtopic_pattern = re.compile(r"^###\s+(.+)")  # Pattern for subtopics
-    list_item_pattern = re.compile(r"^\d+\.\s+(.+)")  # Pattern for list items
-
-    structure = OrderedDict()
-    current_topic = None
-    current_subtopic = None
-
-    for line in content:
-        main_topic_match = main_topic_pattern.match(line)
-        subtopic_match = subtopic_pattern.match(line)
-        list_item_match = list_item_pattern.match(line)
-
-        if main_topic_match:
-            current_topic = main_topic_match.group(1).strip()
-            structure[current_topic] = OrderedDict()
-        elif subtopic_match:
-            current_subtopic = subtopic_match.group(1).strip()
-            structure[current_topic][current_subtopic] = []
-        elif list_item_match:
-            item = list_item_match.group(1).strip()
-            structure[current_topic][current_subtopic].append(item)
-
-    return structure
+def represent_ordereddict(dumper, data):
+    return dumper.represent_dict(data.items())
 
 
-def markdown_to_yaml(markdown_path, yaml_path):
-    structure = parse_markdown(markdown_path)
-    with open(yaml_path, "w") as file:
-        yaml.dump(structure, file, allow_unicode=True)
+def represent_str(dumper, data):
+    if ":" in data or any(c in data for c in "{}[],&*#?|-<>=!%@\\"):
+        return dumper.represent_scalar("tag:yaml.org,2002:str", data, style='"')
+    return dumper.represent_scalar("tag:yaml.org,2002:str", data)
 
 
-def parse_md_to_yaml(md_content):
-    lines = md_content.split("\n")
-    result = []
+yaml.add_representer(OrderedDict, represent_ordereddict)
+yaml.add_representer(str, represent_str)
+
+
+def parse_line(line):
+    """
+    Parses a single line of Markdown and returns its type and content.
+    """
+    if line.startswith("# "):
+        return "main_topic", re.findall(r"^# (.+)$", line)[0]
+    elif line.startswith("## "):
+        return "chapter", re.findall(r"^## (.+)$", line)[0]
+    elif line.startswith("### "):
+        return "subchapter", re.findall(r"^### (.+)$", line)[0]
+    elif re.match(r"^\d+\. ", line):
+        return "content", re.findall(r"^\d+\. (.+)$", line)[0]
+    elif line.strip() == "":
+        return "empty", line  # Handling empty lines
+    else:
+        return "unknown", line
+
+
+def build_yaml_structure(markdown_text):
+    """
+    Converts the Markdown text to a structured YAML format using OrderedDict.
+    """
+    yaml_structure = OrderedDict()
     current_chapter = None
-    current_section = None
+    current_subchapter = None
 
-    for line in lines:
-        if line.startswith("## "):  # New chapter
-            if current_chapter:
-                result.append(current_chapter)
-            current_chapter = {"name": line[3:], "sections": []}
-        elif line.startswith("### "):  # New section
-            if current_section:
-                current_chapter["sections"].append(current_section)
-            current_section = {"name": line[4:], "subsections": []}
-        elif line.startswith("1. "):  # New subsection
-            current_section["subsections"].append(line[3:])
-        else:
+    for line in markdown_text.splitlines():
+        line_type, content = parse_line(line)
+
+        if line_type == "main_topic":
+            main_topic = content.lower().replace(" ", "_")
+            yaml_structure[main_topic] = OrderedDict()
+        elif line_type == "chapter":
+            current_chapter, chapter_name = re.findall(r"^(.+?)\s*:\s*(.+)$", content)[
+                0
+            ]
+            current_chapter = current_chapter.lower().replace(" ", "_")
+            yaml_structure[main_topic][current_chapter] = {
+                "chapter_name": chapter_name,
+                "sub_chapters": OrderedDict(),
+            }
+        elif line_type == "subchapter":
+            current_subchapter, subchapter_name = re.findall(
+                r"^(.+?)\s*:\s*(.+)$", content
+            )[0]
+            subchapter_name = subchapter_name.replace("’", "'")
+            current_subchapter = current_subchapter.lower().replace(" ", "_")
+            yaml_structure[main_topic][current_chapter]["sub_chapters"][
+                current_subchapter
+            ] = OrderedDict([("name", subchapter_name), ("content", [])])
+        elif line_type == "content":
+            yaml_structure[main_topic][current_chapter]["sub_chapters"][
+                current_subchapter
+            ]["content"].append(content.replace("’", "'"))
+        elif line_type == "empty":
             continue
+        elif line_type == "unknown":
+            logger.warning(f"Unknown line type encountered: {content}")
 
-    # Append the last section and chapter
-    if current_section:
-        current_chapter["sections"].append(current_section)
-    if current_chapter:
-        result.append(current_chapter)
-
-    return result
+    return yaml_structure
 
 
-with open("raw_courses/raw_structure.md", "r") as file:
-    md_content = file.read()
-parsed_data = parse_md_to_yaml(md_content)
-with open("output.yaml", "w") as outfile:
-    yaml.dump(parsed_data, outfile, default_flow_style=False)
+def save_to_yaml(data, filename):
+    """
+    Saves the structured data to a YAML file with UTF-8 encoding.
+    """
+    try:
+        with open(filename, "w", encoding="utf-8") as file:
+            yaml.dump(data, file, allow_unicode=True)
+        logger.info(f"YAML file saved successfully: {filename}")
+    except Exception as e:
+        logger.error(f"Error while saving YAML file: {e}")
 
-# Example usage
-# markdown_to_yaml("./raw_courses/raw_structure.md", "structure.yaml")
+
+if __name__ == "__main__":
+    input_filepath = os.path.join(
+        ".", "courses", "seconde", "maths", "structure", "raw_structure.md"
+    )
+    output_filepath = os.path.join(
+        ".", "courses", "seconde", "maths", "structure", "structure.yaml"
+    )
+    with open(input_filepath, "r") as file:
+        md_content = file.read()
+    yaml_data = build_yaml_structure(md_content)
+    save_to_yaml(yaml_data, output_filepath)
