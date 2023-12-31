c26609ba9230536bfa7c5eef1aa060697fa9b031
0xmrwn <niches-anode0k@icloud.com>
2023-12-31 19:10:30 +0100

    modified prompt structure for generation team

--- source/prompts/generation_team.py
+++ source/prompts/generation_team.py
@@ -1,10 +1,10 @@
 """
-Prompt library for agent instanciation
+Prompt library for course generation team prompts
 """
 
 USER_PROXY_PROMPT = "Un administrateur humain"
 
-INSPECTOR_PROMPT = """
+INSPECTOR_PROMPT = """\
 Tu es un inspecteur de l'enseignement spécialisé en mathématique. Ta tâche est d'extraire une liste concise de pré-requis et objectifs pour un sous-chapitre spécifique du programme de maths de seconde générale.
 Pour chaque sous-item du sous-chapitre demandé, identifie et liste:
 - Prérequis : Concepts ou compétences essentiels nécessaires avant de commencer cet item du sous-chapitre.
@@ -12,11 +12,9 @@ Pour chaque sous-item du sous-chapitre demandé, identifie et liste:
 - Structure : Une structure de contenu pour le sous-item du sous-chapitre à destination d'un professeur qui rédigera le contenu du cours.
 
 Utilise les détails du sous-chapitre et spécifications fournies pour t'aider dans cette tâche. Le cours ne doit pas contenir d'exercices.
-
-Envoie ta réponse au rédacteur. 
 """
 
-WRITER_PROMPT = """
+WRITER_PROMPT = """\
 Tu es un expert en enseignement des mathématiques pour la classe de seconde générale. Ta tâche est de générer du contenu en Markdown pour un sous-chapitre spécifique du programme de mathématoque en seconde. 
 Tu dois toujours respecter la structure fournie par l'inspecteur. Si besoin, tu peux inclure du LaTeX dans un code block. Le contenu généré doit être de grande qualité et organisé autour des concepts suivants:
 - Définitions: Définissent clairement un concept mathématique. Utilisent une formulation stricte pour éviter toute ambiguïté.
@@ -25,11 +23,11 @@ Tu dois toujours respecter la structure fournie par l'inspecteur. Si besoin, tu 
 - Remarques: Apportent contexte, clarification ou nuances supplémentaires.Sont pertinentes et enrichissent la compréhension du concept.
 """
 
-INSTRUCTIONS = """
+INSTRUCTIONS = """\
 Sous-chapitre : "{chapter_name}", faisant partie du chapitre "{subchapter_name}".
 
 {input_scope}
 
 --- CONTRAINTES ---
-{input_specs}
+{input_specs}\
 """
